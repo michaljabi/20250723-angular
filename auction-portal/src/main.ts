e9b66743adb9863cfef4d7f29d351d5355272193
Jabi <michal.jabi@gmail.com>
2025-07-23 15:03:23 +0200

    feat(task-05): bootstrap added to project.

--- auction-portal/src/main.ts
+++ auction-portal/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 
 console.log('hello');
