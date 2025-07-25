90a0255bfcb2221aac8cb6486264c40cdee61ae4
Jabi <michal.jabi@gmail.com>
2025-07-25 11:05:18 +0200

    feat(task-15): back-end data interface + service to call data.

---
+++ auction-portal/src/app/auctions/auction-item.ts
@@ -0,0 +1,16 @@
+export interface AuctionItem {
+  id: string;
+  title: string;
+  imgUrl: string;
+  price: number;
+  description?: string;
+}
+
+/*
+ const example = {
+  "id": "3",
+  "title": "Smartfon",
+  "imgUrl": "https://picsum.photos/id/160/600/600",
+  "price": 800
+}
+*/
---
+++ auction-portal/src/app/auctions/auctions-resource.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { AuctionItem } from './auction-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuctionsResourceService {
+  // call to ajax!
+
+  getAll() {}
+
+  addOne(auction: AuctionItem) {}
+}
