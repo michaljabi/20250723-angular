1d57ad92b9d869b21f8afcf9db8c1eac2fde4161
Jabi <michal.jabi@gmail.com>
2025-07-25 10:33:41 +0200

    explain: Using Observables - RxJS -> consumer perspective -> .subsribe

--- auction-portal/src/app/advice/advice-page/advice-details.component.ts
+++ auction-portal/src/app/advice/advice-page/advice-details.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, JsonPipe, UpperCasePipe } from '@angular/common';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   // imports: [UpperCasePipe, JsonPipe],
@@ -32,6 +32,40 @@ export class AdviceDetailsComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.adviceId = this.activatedRoute.snapshot.params['majId'];
     console.log('AdviceDetailsComponent init!');
+
+    // jak odebrać dane ze strumienia?
+    // STREAM CONSUMER
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.adviceId = params['majId'];
+    });
+
+    // konsument nr #2: odbieram i podaje w konsoli
+    this.activatedRoute.params.subscribe((params: Params) => {
+      console.log(params);
+    });
+
+    this.activatedRoute.params.subscribe({
+      next: (params: Params) => {
+        console.log(params);
+      },
+    });
+
+    // Konsument może spodziewać się 3 rzeczy na strumieniu:
+    // 1. odbiór danych (next)
+    // 2. wystąpienie błędu strumienia (error)
+    // 3. zaskończenie emisji (strumienia) (complete)
+
+    // ŻADNA z tych rzeczy - nie może następować równocześnie (1 zasada RxJS)
+    // powyżej interesuje nas tylko NEXT - tylko odbiór danych, nie jesteśmy jako konsument, gotowi na
+    // error / albo / complete
+
+    // żeby zobaczyć pełny obraz, do subscribe musimy podać obiekt z property typu arrow () => {};
+    // API do sprawdzenia 3 rzeczy opisanych powyżej
+    this.activatedRoute.params.subscribe({
+      next: () => {},
+      error: () => {},
+      complete: () => {},
+    });
   }
 
   ngOnDestroy(): void {
