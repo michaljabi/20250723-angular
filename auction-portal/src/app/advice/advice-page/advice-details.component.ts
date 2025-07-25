b0ca7fa904f809fdc03874be468c71d86432a9c5
Jabi <michal.jabi@gmail.com>
2025-07-25 09:22:39 +0200

    feat(task-14): new /advices routing + separate space "feature" division ./advice/

---
+++ auction-portal/src/app/advice/advice-page/advice-details.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  template: `
+    <p>
+      advice-details works!
+    </p>
+  `,
+  styles: ``
+})
+export class AdviceDetailsComponent {
+
+}
--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuctionsPageComponent } from './auctions/auctions-page.component';
 import { PromotionsPageComponent } from './auctions/promotions-page.component';
+import { AdvicePageComponent } from './advice/advice-page/advice-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auctions', pathMatch: 'full' },
@@ -8,4 +9,5 @@ export const routes: Routes = [
   { path: 'promotions', component: PromotionsPageComponent },
   // TODO: pokaż absolutne /aucitons! (child routing)
   // { path: 'hello/world/of/tanks', component: AuctionsPageComponent },
+  { path: 'advices', component: AdvicePageComponent },
 ];
