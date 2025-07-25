fdb33cf21a5cadbbb3eb49da0facad6bb5eb3e55
Jabi <michal.jabi@gmail.com>
2025-07-25 14:06:38 +0200

    explain: dummy component concept.

--- auction-portal/src/app/auctions/auction-card/auction-card.component.ts
+++ auction-portal/src/app/auctions/auction-card/auction-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { AuctionItem } from '../auction-item';
 import { SharedModule } from '../../shared/shared.module';
 
@@ -14,7 +14,7 @@ import { SharedModule } from '../../shared/shared.module';
         <p class="card-text">{{ a?.description }}</p>
         <div class="d-flex justify-content-between align-content-center">
           <strong> {{ a.price }} zł</strong>
-          <button class="btn btn-primary">
+          <button class="btn btn-primary" (click)="addToCart.emit(a)">
             <fa-icon icon="cart-plus"></fa-icon>
           </button>
         </div>
@@ -26,4 +26,7 @@ import { SharedModule } from '../../shared/shared.module';
 export class AuctionCardComponent {
   // props (input)
   auction = input.required<AuctionItem>();
+  addToCart = output<AuctionItem>();
+
+  // tzw. dummy component - nie wie skąd pochodzą dane i nie wie kto (o ile ktoś) je odbiera
 }
