8143ecbedbb050f2ac87794fa423a38259a8454d
Jabi <michal.jabi@gmail.com>
2025-07-25 14:00:58 +0200

    feat(task-18): share module repaired.

--- auction-portal/src/app/shared/shared.module.ts
+++ auction-portal/src/app/shared/shared.module.ts
@@ -1,30 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faGavel, faPlus, faShoppingBasket, faUser, faSearch, faCartPlus, faEdit, faTag, faImage } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGavel,
+  faPlus,
+  faShoppingBasket,
+  faUser,
+  faSearch,
+  faCartPlus,
+  faEdit,
+  faTag,
+  faImage,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { NotificationComponent } from './notification/notification.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    SearchBarComponent,
-    NotificationComponent
-  ],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    FormsModule,
-    // Router config jest Obiektem!
-    // RouterModule.forRoot([], {
-    //
-    // })
-  ],
-  exports: [FontAwesomeModule, SearchBarComponent, NotificationComponent]
+  declarations: [SearchBarComponent, NotificationComponent],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
+  exports: [FontAwesomeModule, SearchBarComponent, NotificationComponent],
 })
 export class SharedModule {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faGavel, faPlus, faShoppingBasket, faUser, faSearch, faCartPlus, faEdit, faTag, faImage);
+    library.addIcons(
+      faGavel,
+      faPlus,
+      faShoppingBasket,
+      faUser,
+      faSearch,
+      faCartPlus,
+      faEdit,
+      faTag,
+      faImage
+    );
   }
 }
